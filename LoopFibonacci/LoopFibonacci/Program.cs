d972b84b3457cfe3378f2208e50857b1a80e9216
zeki tombulu <zekitombulu@gmail.com>
2015-02-22 15:01:54 +0200

    aa_hq1
    loopFibonacci
    recursiveFibonacci

---
+++ LoopFibonacci/LoopFibonacci/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+namespace Fibonacci_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch saat = new Stopwatch(); // stopwatch sınıfının yeni ornegini baslattı.
+            Console.WriteLine("Hesaplanacak Fibonacci sayisini giriniz:");
+            double  sayi = Convert.ToDouble(Console.ReadLine());
+            saat.Start();  //gecen sure icin olcme islemi basladi.
+            Console.WriteLine("Hesaplanan Deger:" + Fibonacci(sayi));
+            saat.Stop(); //gecen sure icin olcme islemi sonlandirildi.
+            TimeSpan zaman = saat.Elapsed; //stopwatch sinifinin elapsed özelliğini kullanarak gecen süre hesaplandi. 
+            string elapsedTime = String.Format("{0}:{1}:{2}.{3}", zaman.Hours, zaman.Minutes, zaman.Seconds, zaman.TotalMilliseconds * 1000000); //gecen saniye milisaniye cinsinden yazilir.
+
+            Console.WriteLine("Calisma Suresi:" + elapsedTime); // çalısma süresini ekrana yazdırıyor.
+            Console.ReadLine(); // sürenin ekranda görünmesini  saglıyor.
+        }
+
+        public static double  Fibonacci(double sayi)
+        {
+            double first = 0, second = 1, next;
+
+            for (double i = 0; i < sayi; i++)
+            {
+               
+                    next = first + second;
+                    first = second;
+                    second = next;
+
+                }
+                return first;
+            }
+
+        }
+    }
---
+++ RecursiveFibonacci/RecursiveFibonacci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Fibanacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch saat = new Stopwatch(); //stopwatch sinifinin yeni bir ornegini baslatir.
+            double sayi;
+            Console.Write("Hesaplanacak Fibonacci sayisini giriniz: ");
+            sayi = Convert.ToDouble(Console.ReadLine());
+            saat.Start(); //hesaplama icin sureyi baslatir.
+            Console.WriteLine("" + Fibonacci(sayi));
+            saat.Stop(); // hesaplama icin sureyi sonlandirir.
+            TimeSpan zaman = saat.Elapsed; //ornek icin gecen toplam sureyi alir.
+            Console.WriteLine("{0}:{1}:{2}.{3}", zaman.Hours, zaman.Minutes, zaman.Seconds, zaman.TotalMilliseconds * 1000000);
+          //  Console.WriteLine("Toplam milisaniye : " + zaman.TotalMilliseconds); // Hesaplanan sureyi milisaniye olarak gosterir.
+            Console.ReadLine(); //ciktinin ekranda gorunmesini saglar
+
+
+
+
+        }
+        static double Fibonacci(double sayi)
+        {
+            if (sayi== 0)
+                return 0;
+            else if (sayi == 1)
+                return 1;
+            else
+                return (Fibonacci(sayi - 1) + Fibonacci(sayi- 2));
+        }
+    }
+}
