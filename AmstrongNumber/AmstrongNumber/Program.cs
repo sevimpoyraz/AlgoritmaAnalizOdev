4437de4b9f287bad104cee3d9d97466f5f7a7d10
Sevim Poyraz <svm.poyraz@gmail.com>
2015-05-17 21:24:36 +0300

    Amstrong Number
    Amstrong Number

---
+++ AmstrongNumber/AmstrongNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Armstrong
+{
+    class ArmstrongNumber
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("100 ile 1000 arasındaki Amstrong Sayıları: ");
+
+            for (int i = 100; i <= 999; i++)
+            {
+                int a = i / 100;
+                int b = (i - a * 100) / 10;
+                int c = (i - a * 100 - b * 10);
+
+                int d = a * a * a + b * b * b + c * c * c;
+
+                if (i == d)
+                    System.Console.WriteLine("{0}", i);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+
+}
