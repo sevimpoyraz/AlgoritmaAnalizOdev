07b1a4f5d7876d391a7a0def3395ceb3e493b3ae
Sevim Poyraz <svm.poyraz@gmail.com>
2015-06-05 12:38:39 +0300

    Final Sinav Sorusu 6
    Final Sinav Sorusu 6

---
+++ SinavSorusu6/SinavSorusu6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SinavSorusu6
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int[] dizi = { 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1 };
+            int sayac = 0;
+            int maxSayac = 0;
+
+             for (int i = 0; i < dizi.Length; i++)
+            {
+                
+                if (dizi[i] == 0)
+                {
+                    
+                    sayac++;
+                    if (i == dizi.Length - 1)
+                    {
+                        if (sayac > maxSayac)
+                        {
+                            maxSayac = sayac;
+                            sayac = 0;
+
+                        }
+                    }
+                    else
+                    {
+                        if (sayac > maxSayac)
+                        {
+                            maxSayac = sayac;
+                            sayac = 0;
+                        }
+                    }
+                
+               }
+            }
+            Console.WriteLine(maxSayac);
+            Console.ReadLine();
+            }
+
+    
+        }
+    }
+
+     
