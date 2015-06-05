1d57bdf7169ff52e91183759dc6e1a641f964247
Sevim Poyraz <svm.poyraz@gmail.com>
2015-06-05 12:39:18 +0300

    Final Sinav Sorusu 5
    Final Sinav Sorusu 5

---
+++ SinavSorusu5/SinavSorusu5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SinavSorusu5
+{
+    class Program
+    {
+
+        public class Node
+        {
+            int  val;
+            Node left;
+            Node right;
+  
+            public static Node root { get; set; }}
+
+       public  static search (Node n){
+            Node p=null;
+            Node node=root;
+            while (node!=null){
+                if(node!=null){
+                    if(node.val>n.val){
+                        p=node;
+                        node=node.left;
+                    }else if(node.val<n.val){
+                        p=node;
+                        node=node.right;
+                    }
+                    else {
+                        return node;
+                    }
+
+                }
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            int val;
+            Node left;
+            Node right;
+        }
+    
+public static Node root { get; set; }}
+}
+
+
